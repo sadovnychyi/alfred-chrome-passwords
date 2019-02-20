0e560c8e66804619a23eb8587d9f5fba3f3d1705
Dmytro Sadovnychyi <git@dmit.ro>
2019-02-20 14:48:51 +0800

    Clean up password from unwanted characters
    Closes #1

--- decrypt.py
+++ decrypt.py
@@ -1,3 +1,4 @@
+import base64
 from Crypto.Cipher import AES
 import hashlib
 import subprocess
@@ -11,7 +12,12 @@ def main(password):
   key = hashlib.pbkdf2_hmac(hash_name='sha1', password=master_password,
                             salt=b'saltysalt', iterations=1003, dklen=16)
   cipher = AES.new(key, AES.MODE_CBC, IV=b' ' * 16)
-  return sys.stdout.write(cipher.decrypt(password.decode('base64')))
+  data = cipher.decrypt(base64.b64decode(password))
+  try:
+    s = ''.join([chr(i) for i in data if i >= 32])
+  except TypeError:
+    s = ''.join([i for i in data if ord(i) >= 32])
+  return sys.stdout.write(s)
 
 
 if __name__ == '__main__':
